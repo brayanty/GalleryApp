039b9c3b66e4e563e3c9484c9bcfd3ab1374c013
Brayan <danielsegoviad@gmail.com>
2019-12-07 04:44:31 -0500

    Add custom style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Blueclaro: "#007BFF",
+        Verde: "#28A745",
+        Naranja: "#FF5733",
+        Blue: "#007BFF",
+        WitherDark: "#FFFFFF",
+        GrayClaroDark: "#B0B0B0",
+        AmarilloClaroDark: "#FFD700",
+      },
+      textColor: {
+        textSubTitleLight: "#000000",
+        textTitleLight: "#333333",
+        textRegularLight: "#003366",
+      },
+      fontFamily: {
+        primaryRegular: ["Regular", "sans-serif"],
+        primaryMedium: ["Medium", "sans-serif"],
+        primarybold: ["Bold", "sans-serif"],
+        titleSemiBold: ["PixelifySans-Semibold"],
+        Filano: ["filano"],
+      },
+    },
   },
   plugins: [],
 };

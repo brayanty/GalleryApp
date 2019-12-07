84bd88646d72959f84878ddbe1fde05b86f25117
Brayan <danielsegoviad@gmail.com>
2019-12-07 04:36:59 -0500

    Add button of save favorite

--- src/components/viewImagen/viewImagen.jsx
+++ src/components/viewImagen/viewImagen.jsx
@@ -1,16 +1,20 @@
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBookmark,
+  faDownload,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PropTypes from "prop-types";
 import { useState } from "react";
 
 ViewImagen.propTypes = {
-  imagenSelect: PropTypes.string.isRequired,
+  imagenSelect: PropTypes.object.isRequired,
 };
 function ViewImagen({ imagenSelect }) {
   const [like, setLike] = useState(false);
 
-  const handlerClick = () => {
+  const handlerClickLike = () => {
     setLike((prenVent) => !prenVent);
   };
 
@@ -19,57 +23,58 @@ function ViewImagen({ imagenSelect }) {
   }
 
   return (
-    <div className="container  w-full mx-auto flex justify-center">
+    <div className="container w-full h-full mx-auto flex justify-center">
       <div className="p-4 m-4 flex gap-3 bg-transparent shadow-md shadow-black rounded-lg">
-        <figure className="md:col-span-2 max-md:row-span-3 overflow-hidden ">
+        <figure className="w-full h-[70vh] md:col-span-2 max-md:row-span-3">
           <img
-            className="w-full h-[70vh]  object-scale-down rounded-2xl"
+            className="w-full h-full  rounded-3xl overflow-hidden"
             src={imagenSelect?.src}
             alt={imagenSelect?.alt}
           />
         </figure>
-        <header className="flex justify-end flex-col">
+        <header className="flex justify-end flex-col gap-2">
           <div className="flex flex-row gap-3 ">
             <button
               className="cursor-pointer p-1 rounded-md"
               onClick={() => {
-                handlerClick();
+                handlerClickLike();
               }}
             >
-              {like ? (
-                <FontAwesomeIcon
-                  style={{ color: "red" }}
-                  icon={faHeart}
-                  size="2x"
-                />
-              ) : (
-                <FontAwesomeIcon icon={faHeart} size="2x" />
-              )}
+              <FontAwesomeIcon
+                className={`${
+                  like
+                    ? "text-red-500"
+                    : "text-textSubTitleLight dark:text-WitherDark"
+                }`}
+                icon={faHeart}
+                size="2x"
+              />
             </button>
-            <button className="p-2 rounded-lg bg-green-600">Donwload</button>
+            <button type="button">
+              <FontAwesomeIcon
+                className={`${
+                  like ? "text-Blueclaro" : "text-black dark:text-WitherDark"
+                }`}
+                icon={faBookmark}
+                size="2x"
+              />
+            </button>
+            <button className="pr-2 pl-2  rounded-lg text-WitherDark bg-green-600 ">
+              <FontAwesomeIcon icon={faDownload} size="2x" />
+            </button>
           </div>
-          <h3 className="font-primarybold text-2xl dark:text-white text-black">
+          <h3 className="font-primarybold text-2xl w-60 text-wrap dark:text-white text-black">
             {imagenSelect?.title}
           </h3>
-          <div className="flex gap-2">
-            {["Waifus", "Venom", "loquesea"].map((item) => {
-              return (
-                <div
-                  className="cursor-pointer transition-all dark:text-white font-primarybold p-1 "
-                  key={item}
-                >
-                  {item}
-                </div>
-              );
-            })}
-          </div>
           <div className="flex flex-col justify-center gap-10">
             <div className="flex flex-row gap-1 justify-between items-center">
               <div className="flex items-center gap-2">
                 <div className="p-8 bg-slate-300 rounded-full"></div>
                 <header className="dark:text-white">
                   <h4 className="font-primarybold">Brayan Palacios</h4>
-                  <span>Artista</span>
+                  <span className="text-textSubTitleLight dark:text-WitherDark">
+                    Artista
+                  </span>
                 </header>
               </div>
             </div>

3c3c7e12721358671f5ab83463df0682e0fe8aed
Brayan <danielsegoviad@gmail.com>
2024-10-10 11:06:04 -0500

    Add component tags

---
+++ src/components/tags/tags.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const tags = ["Inicio", "Explorar"];
+
+function Tags() {
+  const [tagsSelect, setTagsSelect] = useState(null);
+  if (!tags) {
+    return;
+  }
+  const handlerTagsSelect = (e) => {
+    setTagsSelect(e.target.innerText);
+  };
+  return (
+    <div className="p-3 mb-2">
+      <nav className="flex gap-3">
+        {tags.map((tag) => (
+          <div
+            onClick={handlerTagsSelect}
+            className={`${
+              tagsSelect === tag ? "border-b-4 border-Blueclaro" : ""
+            } flex cursor-pointer transition-all text-black dark:text-white hover:text-Blue hover:dark:text-Blue font-primarybold`}
+            key={tag}
+          >
+            {tag.charAt(0).toUpperCase() + tag.slice(1)}
+          </div>
+        ))}
+      </nav>
+    </div>
+  );
+}
+
+export default Tags;

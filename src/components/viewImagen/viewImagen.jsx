ce493d75b46f827178b49850eeaab738d6adef95
Brayan <danielsegoviad@gmail.com>
2024-10-18 00:17:34 -0500

    Changes tags

--- src/components/viewImagen/viewImagen.jsx
+++ src/components/viewImagen/viewImagen.jsx
@@ -1,4 +1,3 @@
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBookmark,
   faDownload,
@@ -9,72 +8,112 @@ import PropTypes from "prop-types";
 import { useState } from "react";
 
 ViewImagen.propTypes = {
-  imagenSelect: PropTypes.object.isRequired,
+  imagenSelect: PropTypes.object,
 };
 function ViewImagen({ imagenSelect }) {
   const [like, setLike] = useState(false);
-
-  const handlerClickLike = () => {
-    setLike((prenVent) => !prenVent);
-  };
+  const [likeCount, setLikeCount] = useState(0);
+  const [favorite, setFavorite] = useState(false);
+  const [favoriteCount, setFavoriteCount] = useState(0);
 
   if (imagenSelect == " ") {
     return;
   }
 
+  const handlerLike = () => {
+    if (likeCount >= 10) {
+      return;
+    }
+    if (likeCount == 7) {
+      setTimeout(() => {
+        setLikeCount(0);
+      }, 10000);
+    }
+
+    setLike((prenVent) => !prenVent);
+    setLikeCount((countLikes) => (countLikes = countLikes + 1));
+  };
+
+  const handlerFavorite = () => {
+    if (favoriteCount >= 10) {
+      return;
+    }
+    if (favoriteCount == 7) {
+      setTimeout(() => {
+        setFavoriteCount(0);
+      }, 10000);
+    }
+    setFavorite((prenVent) => !prenVent);
+    setFavoriteCount((countFavorite) => (countFavorite = countFavorite + 1));
+  };
+
   return (
-    <div className="container w-full h-full mx-auto flex justify-center">
-      <div className="p-4 m-4 flex gap-3 bg-transparent shadow-md shadow-black rounded-lg">
-        <figure className="w-full h-[70vh] md:col-span-2 max-md:row-span-3">
+    <div
+      id="viewImagen"
+      className="container w-full h-full mx-auto flex justify-center"
+    >
+      <div className="w-full h-full p-4 md:m-2 flex flex-col gap-3 bg-transparent md:shadow-md md:shadow-black rounded-lg">
+        <figure className="max-md:h-full h-[70vh] w-auto md:col-span-2 max-md:row-span-3">
           <img
-            className="w-full h-full  rounded-3xl overflow-hidden"
+            className="w-full h-full object-scale-down max-md:object-cover rounded-3xl overflow-hidden"
             src={imagenSelect?.src}
             alt={imagenSelect?.alt}
           />
         </figure>
-        <header className="flex justify-end flex-col gap-2">
-          <div className="flex flex-row gap-3 ">
-            <button
-              className="cursor-pointer p-1 rounded-md"
-              onClick={() => {
-                handlerClickLike();
-              }}
-            >
-              <FontAwesomeIcon
-                className={`${
-                  like
-                    ? "text-red-500"
-                    : "text-textSubTitleLight dark:text-WitherDark"
-                }`}
-                icon={faHeart}
-                size="2x"
-              />
-            </button>
-            <button type="button">
-              <FontAwesomeIcon
-                className={`${
-                  like ? "text-Blueclaro" : "text-black dark:text-WitherDark"
-                }`}
-                icon={faBookmark}
-                size="2x"
-              />
-            </button>
-            <button className="pr-2 pl-2  rounded-lg text-WitherDark bg-green-600 ">
-              <FontAwesomeIcon icon={faDownload} size="2x" />
-            </button>
+        <header className="flex justify-between flex-col gap-3">
+          <div className="flex justify-between gap-3 pr-2 pl-2s">
+            <h3 className="font-primarybold text-2xl w-60 text-wrap dark:text-white text-black">
+              {imagenSelect?.title}
+            </h3>
+            <div className="flex flex-row gap-4">
+              <button
+                className="cursor-pointer p-1 rounded-md"
+                onClick={() => {
+                  handlerLike();
+                }}
+              >
+                <FontAwesomeIcon
+                  className={`${
+                    like
+                      ? "text-red-500"
+                      : "text-textSubTitleLight dark:text-WitherDark"
+                  }`}
+                  icon={faHeart}
+                  size="2x"
+                />
+              </button>
+              <button
+                className="cursor-pointer p-1 rounded-md"
+                type="button"
+                onClick={() => {
+                  handlerFavorite();
+                }}
+              >
+                <FontAwesomeIcon
+                  className={` ${
+                    favorite
+                      ? "text-Blueclaro"
+                      : "text-black dark:text-WitherDark"
+                  }`}
+                  icon={faBookmark}
+                  size="2x"
+                />
+              </button>
+              <button>
+                <FontAwesomeIcon
+                  className="text-green-600 dark:text-green-500"
+                  icon={faDownload}
+                  size="2x"
+                />
+              </button>
+            </div>
           </div>
-          <h3 className="font-primarybold text-2xl w-60 text-wrap dark:text-white text-black">
-            {imagenSelect?.title}
-          </h3>
           <div className="flex flex-col justify-center gap-10">
             <div className="flex flex-row gap-1 justify-between items-center">
               <div className="flex items-center gap-2">
-                <div className="p-8 bg-slate-300 rounded-full"></div>
+                <div className="p-4 bg-slate-300 rounded-full"></div>
                 <header className="dark:text-white">
-                  <h4 className="font-primarybold">Brayan Palacios</h4>
-                  <span className="text-textSubTitleLight dark:text-WitherDark">
-                    Artista
-                  </span>
+                  <h4 className="font-primarybold text-xs">Brayan Palacios</h4>
                 </header>
               </div>
             </div>

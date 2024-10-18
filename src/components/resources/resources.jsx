0df2113529b20989fbea63165ca287bddc104675
Brayan <danielsegoviad@gmail.com>
2024-10-18 00:19:31 -0500

    Add handlerUserView

--- src/components/resources/resources.jsx
+++ src/components/resources/resources.jsx
@@ -7,31 +7,31 @@ RenderCard.propTypes = {
   foto: PropTypes.object.isRequired,
   index: PropTypes.number.isRequired,
   handlerClick: PropTypes.func.isRequired,
+  handlerUserView: PropTypes.func.isRequired,
 };
 
-export function RenderCard({ foto, index, handlerClick }) {
+export function RenderCard({ foto, index, handlerClick, handlerUserView }) {
   const [loading, setLoading] = useState(true);
 
   const handleImageLoad = () => {
     setLoading(false);
   };
 
-  const getRowSpan = (index) =>
-    index % 2 === 0 ? "md:row-span-2 max-md:pb-4" : "md:row-span-4";
+  const getRowSpan = (index) => {
+    return index % 2 === 0 ? "270px" : index % 3 === 0 ? "270px" : "330px";
+  };
 
   return (
-    <div
-      className={`${getRowSpan(
-        index
-      )} max-md:w-full max-md:h-[350px] row-span-2 flex flex-col gap-3 items-center`}
+    <li
+      className={`bg-slate-500/30 rounded-xl p-2 flex flex-col gap-2 items-center`}
     >
-      <div className="relative w-full h-full">
+      <div className="w-full h-full relative" onClick={() => handlerClick(index)}>
         <figure
-          onClick={() => handlerClick(index)}
-          className="h-full w-full cursor-pointer"
+          className="cursor-pointer"
+          style={{ height: getRowSpan(index), width: "100%" }}
         >
           {loading && (
-            <div className="absolute inset-0 flex items-center justify-center bg-gray-200">
+            <div className="absolute p-4 inset-0 flex items-center justify-center">
               <div className="lds-ellipsis">
                 <div></div>
                 <div></div>
@@ -41,46 +41,41 @@ export function RenderCard({ foto, index, handlerClick }) {
             </div>
           )}
           <img
-            className="w-full h-full rounded-lg object-cover object-right-top"
+            className="w-full h-full rounded-lg object-cover object-center transition-transform ease-in-out duration-300 hover:scale-105"
             src={foto.src}
             alt={foto.alt}
             onLoad={handleImageLoad}
-            onError={() => setLoading(false)} // Maneja el error en caso de que la imagen no se cargue
+            onError={() => setLoading(false)}
           />
         </figure>
       </div>
-      <div className="justify-center bottom-0 flex flex-col p-1 items-center">
-        {/* Components users todavia no lo voy a arreglar */}
-        <div className="w-full flex gap-3 items-center ">
-          <div className="flex gap-1 justify-center items-center">
-            <figure>
-              <img
-                className="rounded-full w-10"
-                src="https://randomuser.me/api/portraits/thumb/men/31.jpg"
-                alt="Usurio"
-              />
-            </figure>
-            <header className="dark:text-white">
-              <h4 className="font-primaryMedium text-sm">Brayan Palacios</h4>
-              <div className="font-primaryRegular text-xs text-textSubTitleLight dark:text-WitherDark">
-                Artistas
-              </div>
-            </header>
-          </div>
-          <div>
-            <button className="font-primaryMedium p-1 rounded-md bg-slate-300 border border-gray-900">
-              Seguir
-            </button>
-          </div>
+      <div className="self-start">
+        <h5 className="text-textSubTitleLights dark:text-WitherDark font-primaryMedium text-lg max-md:text-xs">
+          {foto.title}
+        </h5>
+      </div>
+      <div className="self-start flex flex-col items-center justify-center">
+        <div className="flex gap-1 justify-center items-center">
+          <div className="p-4 bg-slate-300 rounded-full"></div>
+          <header className="flex gap-1 justify-around dark:text-white">
+            <h4
+              onClick={() => {
+                handlerUserView(foto.idUser);
+              }}
+              className="cursor-pointer hover:text-blue-300 font-primaryMedium text-sm max-md:text-xs"
+            >
+              Brayan Palacios
+            </h4>
+          </header>
         </div>
       </div>
-    </div>
+    </li>
   );
 }
 
 export function RenderSearch() {
   return (
-    <div className="w-full max-w-[600px] inline-block">
+    <div className="w-full max-w-[400px] inline-block">
       <form
         method="get"
         id="formSearch"

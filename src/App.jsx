46ef74f2e25926e9cbd1cf62343005f55f353e24
Brayan <danielsegoviad@gmail.com>
2024-10-18 00:12:12 -0500

    Add componets a componets App

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import "./App.css";
 import Navbar from "./components/navbar/nav";
 import NavbarMobile from "./components/navbar/navMobile";
 import ViewImagen from "./components/viewImagen/viewImagen";
-import MainImgs from "./components/MainImgs/MainImgs.jsx";
+import MainImgs from "./components/MainImgs/MainImgs";
 import Footer from "./components/footer/footer";
 import Login from "./components/login/login";
-import Tags from "./components/tags/tags.jsx";
+import UploadImagen from "./components/uploadImg/upload";
+import Tags from "./components/tags/tags";
+// import Galery from "./components/miniGalery/galery";
+import Profile from "./components/profile/profile";
+import CardMoreUsers from "./components/cardMoreUsers/cardMoreUsers.jsx";
+
 import { useDarkLightMode } from "./components/logic/darkMode";
 import { useEffect, useState } from "react";
 
@@ -14,6 +19,8 @@ function App() {
   const [imagenSelect, setimagenSelect] = useState(" ");
   const [visibleLogin, SetVisibleLogin] = useState(false);
   const [mobileOn, setMobileOn] = useState(true);
+  const [isUploadImg, setUploadImg] = useState(false);
+  const [profileView, setProfileView] = useState(" ");
 
   useEffect(() => {
     if (window.innerWidth > 768) {
@@ -33,32 +40,56 @@ function App() {
     setimagenSelect(img);
   };
 
-  const handlerLogin = () => {
+  const LoginVisible = () => {
     SetVisibleLogin((prenVent) => !prenVent);
+  };
+
+  const selectLogin = () => {
+    SetVisibleLogin((prenVent) => !prenVent);
+  };
+
+  const handlerUserView = (IdUser) => {
+    setProfileView(IdUser);
+  };
+
+  const uploadImgVisible = () => {
+    setUploadImg((prenVent) => !prenVent);
   };
 
   return (
     <>
       {mobileOn ? (
         <>
-          <NavbarMobile
-            theme={theme}
-            handlerClickDark={handlerClickDark}
-            handlerLogin={handlerLogin}
-          />
+          <NavbarMobile LoginVisible={LoginVisible} />
         </>
       ) : (
         <Navbar
           theme={theme}
           handlerClickDark={handlerClickDark}
-          handlerLogin={handlerLogin}
+          LoginVisible={LoginVisible}
+          selectLogin={selectLogin}
+          uploadImgVisible={uploadImgVisible}
         />
       )}
+      <Profile profileView={profileView} />
       <ViewImagen imagenSelect={imagenSelect} />
-      <Tags />
-      <Login visible={visibleLogin} handlerLogin={handlerLogin} />
-      <MainImgs handlerSelect={handlerImagenSelect} items={"items"} />
+      <div className="grid md:grid-cols-4 grid-cols-1">
+        <div className="col-span-3 w-full">
+          <Tags />
+          <MainImgs
+            handlerSelect={handlerImagenSelect}
+            handlerUserView={handlerUserView}
+            items={"items"}
+          />
+        </div>
+        <div className="w-[300px] h-[400px]">
+          <CardMoreUsers />
+        </div>
+      </div>
+
       <Footer />
+      <UploadImagen visible={isUploadImg} closeModall={uploadImgVisible} />
+      <Login visible={visibleLogin} LoginVisible={LoginVisible} />
     </>
   );
 }

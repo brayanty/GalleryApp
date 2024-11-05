791fd6695a9f357d3e1ff20ea3ddf7b842304ebf
Brayan <danielsegoviad@gmail.com>
2024-11-05 17:51:04 -0500

    Add alias de ruta

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +8,13 @@ export default defineConfig({
     host: "0.0.0.0",
   },
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@img": path.resolve(__dirname, "./src/img"),
+    },
+  },
 });

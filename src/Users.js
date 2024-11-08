ae9bb8fd476bea1d6adef57775bef623c7689ed1
Brayan linux <2003brayan2@gmail.com>
2024-11-08 18:33:42 -0500

    add data file

---
+++ src/Users.js
@@ -0,0 +1,34 @@
+const Users = [
+  {
+    name: "Brayan Palacios",
+    bg: "https://images.pexels.com/photos/735423/pexels-photo-735423.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    profile:
+      "https://st5.depositphotos.com/21922568/66247/v/450/depositphotos_662472280-stock-illustration-young-man-anime-style-character.jpg",
+    id: "Elbrayanp",
+    subTitle: "Creador de comics",
+    followers: "23.4k",
+    tags: ["anime", "mascotas", "naturaleza", "videojuegos"],
+  },
+  {
+    name: "Raimer nunez",
+    bg: "https://images.pexels.com/photos/10945422/pexels-photo-10945422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    profile:
+      "https://www.shutterstock.com/image-vector/young-man-anime-style-character-600nw-2240084847.jpg",
+    id: "1",
+    subTitle: "Creador de comics, y creador de cosas",
+    followers: "223.4k",
+    tags: ["anime", "mascotas", "naturaleza", "videojuegos"],
+  },
+  {
+    name: "Juana laya",
+    bg: "https://images.pexels.com/photos/24740473/pexels-photo-24740473/free-photo-of-mujer-ordenador-portatil-cama-modelo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    profile:
+      "https://www.shutterstock.com/image-vector/young-man-anime-style-character-600nw-2240084847.jpg",
+    id: "5",
+    subTitle: "Creador de comics, y creador de cosas",
+    followers: "223.4k",
+    tags: ["anime", "mascotas", "naturaleza", "videojuegos"],
+  },
+];
+
+export default Users;
